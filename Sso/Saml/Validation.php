f7503f56ed21f24dd6dd58a73f40ca579651a718
Douglas Teoh <douglas@dteoh.com>
2014-03-04 11:28:30 +1000

    Fix class lookup error.

--- Sso/Saml/Validation.php
+++ Sso/Saml/Validation.php
@@ -5,6 +5,7 @@ namespace BeSimple\SsoAuthBundle\Sso\Saml;
 use BeSimple\SsoAuthBundle\Sso\AbstractValidation;
 use BeSimple\SsoAuthBundle\Sso\ValidationInterface;
 use Buzz\Message\Response;
+use OneLogin_Saml_Response as SamlResponse;
 use OneLogin_Saml_Settings as SamlSettings;
 
 class Validation extends AbstractValidation implements ValidationInterface
@@ -20,7 +21,7 @@ class Validation extends AbstractValidation implements ValidationInterface
     protected function validateResponse(Response $response)
     {
         $assertion = $this->getCredentials();
-        $samlResponse = new OneLogin_Saml_Response($this->samlSettings, $assertion);
+        $samlResponse = new SamlResponse($this->samlSettings, $assertion);
 
         $success = false;
 
