81087de7558f3d0fefa4b4c634e2d9603fd4e9f5
Douglas Teoh <douglas@dteoh.com>
2014-03-04 11:02:06 +1000

    Add AbstractValidation implementation for SAML credentials.

---
+++ Sso/Saml/Validation.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BeSimple\SsoAuthBundle\Sso\Saml;
+
+use BeSimple\SsoAuthBundle\Sso\AbstractValidation;
+use BeSimple\SsoAuthBundle\Sso\ValidationInterface;
+use Buzz\Message\Response;
+use OneLogin_Saml_Settings as SamlSettings;
+
+class Validation extends AbstractValidation implements ValidationInterface
+{
+
+    private $samlSettings;
+
+    public function setSamlSettings(SamlSettings $settings)
+    {
+        $this->samlSettings = $settings;
+    }
+
+    protected function validateResponse(Response $response)
+    {
+        $assertion = $this->getCredentials();
+        $samlResponse = new OneLogin_Saml_Response($this->samlSettings, $assertion);
+
+        $success = false;
+
+        try {
+            $success = $samlResponse->isValid();
+        } catch (\Exception $ex) {
+            $this->error = $ex->getMessage();
+        }
+
+        if ($success) {
+            $this->username = $samlResponse->getNamedId();
+            $this->attributes = $samlResponse->getAttributes();
+        }
+
+        return $success;
+    }
+
+}
