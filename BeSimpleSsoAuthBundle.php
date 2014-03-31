61faed99ce94591976218a8b8661fb8bca7f32f1
Douglas Teoh <douglas@dteoh.com>
2014-03-31 11:25:34 +1000

    Add missing SamlSsoFactory import.

--- BeSimpleSsoAuthBundle.php
+++ BeSimpleSsoAuthBundle.php
@@ -2,11 +2,12 @@
 
 namespace BeSimple\SsoAuthBundle;
 
-use BeSimple\SsoAuthBundle\DependencyInjection\Security\Factory\TrustedSsoFactory;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Kernel;
 use BeSimple\SsoAuthBundle\DependencyInjection\Compiler\FactoryPass;
+use BeSimple\SsoAuthBundle\DependencyInjection\Security\Factory\SamlSsoFactory;
+use BeSimple\SsoAuthBundle\DependencyInjection\Security\Factory\TrustedSsoFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * @author: Jean-François Simon <contact@jfsimon.fr>
